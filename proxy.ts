a9cfb86bdf6bff01f880f3e1b4190bee6ed29630
muez ashour <muazashour@gmail.com>
2025-11-05 12:39:56 +0300

    migrated middleware to proxy and fixed package.json

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export default function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   const publicPaths = ["/sign-in", "/sign-up"];
